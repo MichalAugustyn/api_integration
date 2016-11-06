ffa2a0268400af086a267a624adcf574308a3f5e
Michal Augustyn <michaltptk@gmail.com>
2016-11-06 12:08:07 +0100

    Added api requirements

---
+++ firstboot.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install git
+sudo apt-get install python-pip
+sudo apt-get install mysql-server
+sudo apt-get install mysql-connector
+sudo apt-get install python-mysqldb
+
+sudo pip install python-mysql
+sudo pip install flask
+sudo pip install python-flask
+sudo pip install flask-restful
+sudo pip install flask-mysql
+sudo pip install python-simplexml
+sudo pip install requests
+sudo pip install mysql-connector
+
